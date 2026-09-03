689f34176ed668a86d9b641be2c0e74b12e31034
Ethan Cox <ethanmlcox@gmail.com>
2026-09-02 19:54:15 -0600

    Resolve ESM compatibility error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 module.exports = {
   content: ['index.html', './src/**/*.{html,js,tsx}', './node_modules/preline/preline.js'],
